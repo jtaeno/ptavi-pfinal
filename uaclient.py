b0eb4833a2af7ffb562b03ef0af4ece6c2dac54f
jtaeno <juliotaeno@yahoo.es>
2019-01-10 15:24:36 +0100

    mejora programa uaclient.py y uaserver.py todavia sin finalizar

--- uaclient.py
+++ uaclient.py
@@ -2,9 +2,10 @@ import socket
 import sys
 import os
 
-from proxy import LOG
+from proxy import LOG, contra
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+
 
 class XmlHandler(ContentHandler):
 
@@ -56,6 +57,7 @@ if __name__ == "__main__":
         audio_path = DICCIONARIO[5]['path']
 
         LINEA = ""
+        texto = ''
         METODO = Metod.upper()
         V = 'v = 0\r\n'
         O = 'o = ' + account_username + ' ' + uaserver_ip + '\r\n'
@@ -68,6 +70,7 @@ if __name__ == "__main__":
             my_socket.connect((regproxy_ip, regproxy_puerto))
 
             if METODO == 'REGISTER':
+                LOG.fich_log(log_path, "starting",regproxy_ip , regproxy_puerto, texto)
                 LINE = 'REGISTER ' + 'sip:' + account_username + ':'
                 LINE += str(uaserver_puerto) + ' SIP/2.0\r\n'
                 LINE += 'Expires: ' + Opcion + '\r\n'
@@ -83,29 +86,31 @@ if __name__ == "__main__":
 
             print("Enviando: " + LINE + "\r\n")
             my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-            texto = LINE.split("\r\n")
-            LOG.fich_log(log_path, "sent_to",
-                        regproxy_ip , regproxy_puerto, texto)
-
-            data = my_socket.recv(1024)
+            texto = " ".join( LINE.split("\r\n"))
+            LOG.fich_log(log_path, "sent_to", regproxy_ip , regproxy_puerto, texto)
+            try:
+                data = my_socket.recv(1024)
+            except ConnectionRefusedError or ConnectionResetError:
+                texto = "ERROR: No Connection"
+                LOG.fich_log(log_path, "received",
+                            regproxy_ip , regproxy_puerto, texto)
+                sys.exit("ERROR: No Connection")
             respuesta = data.decode('utf-8')
             resultado = data.decode('utf-8').split('\r\n')
             resultado1 = data.decode('utf-8').split(' ')
             supuestos = "SIP/2.0 100 Trying"
-
-            texto = respuesta.split('\r\n')
-
+            texto = " ".join(respuesta.split('\r\n'))
             LOG.fich_log(log_path, "received",
-                        regproxy_ip, regproxy_puerto, resultado)
-
-            if resultado1[0] == "SIP/2.0 100 Trying\r\n":
-                respuesta = "SIP/2.0 100 Trying\r\n"
-                respuesta += "SIP/2.0 180 Ringing\r\n"
-                respuesta += "SIP/2.0 200 OK\r\n"
+                        regproxy_ip, regproxy_puerto, texto)
             if "BYE RECIBIDO" in respuesta:
+                texto = ''
+                LOG.fich_log(log_path, "finishing", regproxy_ip , regproxy_puerto, texto)
                 respuesta  = respuesta
-            if respuesta == "SIP/2.0 405 Method Not Allowed\r\n":
+            if  "405" in respuesta:
+                texto = ' '.join(respuesta.split("\r\n"))
                 respuesta =  "METODO INCORRECTO\r\n" + respuesta
+                LOG.fich_log(log_path, "error",
+                            regproxy_ip , regproxy_puerto, texto)
             if  resultado1[1] == '401':
                 respuesta = "/// NECESITA AUTORIZACION ///\r\n" + respuesta
 
@@ -113,34 +118,27 @@ if __name__ == "__main__":
             print(respuesta + '\r\n')
 
             if  "401" in respuesta:
-                contraseña =  respuesta.split(' ')
-                contraseña_regis = respuesta.split(' ')[10].split('\r\n\r\n')[0]
-
+                contraseña_regis = respuesta.split(' ')[13].split('\r\n\r\n')[0]
+                nonce = contra(account_passwd, contraseña_regis)
                 LINE = 'REGISTER ' + 'sip:' + account_username + ':'
                 LINE += str(uaserver_puerto) + ' SIP/2.0\r\n'
                 LINE += 'Expires: ' + Opcion + '\r\n'
-                LINE += 'Authorization: Digest response = ' + contraseña_regis
-
-                texto = LINE.split("\r\n")
+                LINE += 'Authorization: Digest response = ' + nonce
+                texto = ' '.join( LINE.split("\r\n"))
                 LOG.fich_log(log_path, "sent_to",
                             regproxy_ip , regproxy_puerto, texto)
                 print("Enviando: " + LINE + "\r\n")
                 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-
-            if supuestos == resultado[0]:
+            if supuestos == resultado[1]:
                 print("Enviando ACK...", 'ACK sip:' + Opcion + ' SIP/2.0')
-                ACK = ['ACK sip: ' + Opcion + ' SIP/2.0', 'utf-8']
-                texto = ACK
+                ACK = 'ACK sip: ' + Opcion + ' SIP/2.0', 'utf-8'
+                texto = str(ACK)
                 LOG.fich_log(log_path, "sent_to",
                             regproxy_ip , regproxy_puerto, texto)
                 my_socket.send(bytes('ACK sip: ' + Opcion + ' SIP/2.0', 'utf-8') +
                                b'\r\n')
-    except ConnectionRefusedError:
-        texto = ["ERROR: No Connection"]
-        LOG.fich_log(log_path, "received",
-                    regproxy_ip , regproxy_puerto, texto)
-        sys.exit("ERROR: No Connection")
+
     except FileNotFoundError:
-        sys.exit("Usage: python3 karaoke.py file.smil.")
+        sys.exit("Usage: python3 .xml file")
     except ValueError:
         sys.exit("Usage: python uaclient.py config method option")
--- uaserver.py
+++ uaserver.py
@@ -10,56 +10,58 @@ from proxy import LOG
 class EchoHandler(socketserver.DatagramRequestHandler):
     info_invite = {'IP' : '' , 'PUERTO' : ''}
     def handle(self):
+        texto = ''
         ip_client = self.client_address[0]
         puerto_client = self.client_address[1]
         while 1:
             line = self.rfile.read().decode('utf-8')
-            print("El cliente nos manda " + line)
-            METODO = line.split()[0]
+            print("El cliente nos manda: " + '\r\n' + line)
+            METODO = line.split()[6]
             recibido =  line.split()
-            texto = line.split('\r\n')
+            texto = ' '.join(line.split("\r\n"))
             LOG.fich_log(log_path, "received",
                         regproxy_ip, regproxy_puerto, texto)
-
             if METODO == 'INVITE':
-                self.info_invite['IP'] = recibido[11]
-                self.info_invite['PUERTO'] = recibido[21]
-                texto = line.split("\r\n")
-                LOG.fich_log(log_path, "sent_to",
-                            regproxy_ip, regproxy_puerto, texto)
-
+                self.info_invite['IP'] = recibido[17]
+                self.info_invite['PUERTO'] = recibido[27]
                 V = 'v = 0\r\n'
                 O = 'o = ' +  recibido[10] + ' ' + recibido[11] + '\r\n'
                 S = 's = misesion' + '\r\n'
                 T = 't = 0' + '\r\n'
                 M = 'm = audio ' + recibido[21] + ' RTP\r\n'
                 SDP = 'Content-Type: application/sdp\r\n' + V + O + S + T + M
-
+                enviar = "SIP/2.0 100 Trying\r\n" + "SIP/2.0 180 Ringing\r\n" + "SIP/2.0 200 OK\r\n" + SDP
                 self.wfile.write(b"SIP/2.0 100 Trying\r\n")
                 self.wfile.write(b"SIP/2.0 180 Ringing\r\n")
                 self.wfile.write(b"SIP/2.0 200 OK\r\n")
                 self.wfile.write(bytes(SDP, 'utf-8'))
+                texto = ' '.join(enviar.split("\r\n"))
+                LOG.fich_log(log_path, "sent_to",
+                            regproxy_ip, regproxy_puerto, texto)
+                print("Enviamos: " + '\r\n' + enviar)
             elif METODO == 'ACK':
+                aEjecutarVLC = 'cvlc rtp://@' + self.info_invite['IP'] + ':' + self.info_invite['PUERTO']
+                print("Vamos a ejecutar", aEjecutarVLC)
+                os.system(aEjecutarVLC + '&')
                 aEjecutar = "./mp32rtp -i " + self.info_invite['IP'] + " -p " + self.info_invite['PUERTO']
                 aEjecutar += " < " + audio_path
                 print("Vamos a ejecutar", aEjecutar)
                 os.system(aEjecutar)
             elif METODO == 'BYE':
-                self.wfile.write(b"BYE RECIBIDO\r\n SIP/2.0 200 OK \r\n")
+                self.wfile.write(b"BYE RECIBIDO\r\nSIP/2.0 200 OK \r\n")
                 print("SIP/2.0 200 OK \r\n\r\n")
-                texto = line.split("\r\n")
+                texto = ' '.join(line.split("\r\n"))
                 LOG.fich_log(log_path, "sent_to",
                             regproxy_ip, regproxy_puerto, texto)
 
             elif METODO != ['INVITE', 'ACK', ' BYE']:
                 self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n")
-                texto = line.split('\r\n')
-                texto = line.split("\r\n")
+                texto = ' '.join(line.split("\r\n"))
                 LOG.fich_log(log_path, "sent_to",
                             regproxy_ip, regproxy_puerto, texto)
+            else:
                 self.wfile.write(b"SIP/2.0 400 Bad Request\r\n")
-                texto = line.split("\r\n")
-                texto = line.split("\r\n")
+                texto = ' '.join(line.split("\r\n"))
                 LOG.fich_log(log_path, "sent_to",
                             regproxy_ip, regproxy_puerto, texto)
 
